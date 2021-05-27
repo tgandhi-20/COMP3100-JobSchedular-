05faf3efb53f09855d449e19c8c5e56af347591e
tgandhi_20 <tanay-bhaskar.gandhi@students.mq.edu.au>
2021-05-27 11:24:06 +1000

    Updated server class to match the GETS Capable info

--- Server.java
+++ Server.java
@@ -1,22 +1,24 @@
 public class Server {
-    protected int id;
     protected String type;
-    protected int limit;
-    protected int bootUpTime;
-    protected float hourlyRate;
+    protected int id;
+    protected String state;
+    protected int curStartTime ;
     protected int core;
     protected int memory;
     protected int disk;
+    protected int wjobs;
+    protected int rjobs;
 
-    public Server(int id, String t, int l, int b, float hr, int c, int m, int d) {
-        this.id = id;
+    public Server( String t, int id, String s, int cst, int c, int m, int d,int wj, int rj) {
         this.type = t;
-        this.limit = l;
-        this.bootUpTime = b;
-        this.hourlyRate = hr;
+        this.id = id;
+        this.state= s ;
+        this.curStartTime = cst;
         this.core = c;
         this.memory = m;
         this.disk = d;
+        this.wjobs=wj;
+        this.rjobs=rj;
 
     }
 }
