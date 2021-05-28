6c23b7476644c9881c771b3c800fba80b9066af8
tgandhi_20 <tanay-bhaskar.gandhi@students.mq.edu.au>
2021-05-28 15:10:20 +1000

    Updated the scheduling algorithm and main method
    previous version had problems with test file executable provided. Updated the main method to solve the problem and also updated scheduling algorithm to increase resource utilization.

--- Client.java
+++ Client.java
@@ -2,147 +2,143 @@ import java.net.*;
 import java.util.*;
 import java.io.*;
 
-
 public class Client {
-   
+
+    // s
     private static Socket s;
- 
+
     // s args
     private static final String hostname = "localhost";
     private static final int serverPort = 50000;
- 
+
     // streams
     private static BufferedReader bfr;
     private static InputStreamReader din;
     private static DataOutputStream dout;
- 
+
     // commands
-    private static final String HELO = "HELO\n";
-    private static final String OK = "OK\n";
+    private static final String HELO = "HELO";
+    private static final String OK = "OK";
     private static final String AUTH = "AUTH";
-    private static final String REDY = "REDY\n";
+    private static final String REDY = "REDY";
     private static final String JOBN = "JOBN";
     private static final String JCPL = "JCPL";
+    private static final String GETS = "GETS";
     private static final String SCHD = "SCHD";
+    private static final String EJWT = "EJWT";
     private static final String NONE = "NONE";
-    private static final String QUIT = "QUIT\n";
+    private static final String QUIT = "QUIT";
 
     // buffer fields
-   
-    private static byte[] byteBuffer; // will hold the current message from the server stored as bytes
- 
     private static String stringBuffer; /* will hold the current message from the server stored in a string
                                                                        (created from charArray)        */
     private static String[] fieldBuffer; /* will hold the current message from the server as an array of strings
-                                                                       (created from stringBuffer)     */  
+                                                                       (created from stringBuffer)     */
+
     private static String scheduleString; // string to be scheduled
 
     // create server/list objects
-    private static List<Server> serverList;
     
- 
- 
+
     public static void main(String[] args) throws IOException {
         setup();
- 
+
         try {
             writeBytes(HELO); // client sends HELO
- 
-            // server replies with OK
- 
-            System.out.println("sent AUTH username");
-            writeBytes(AUTH + " " + System.getProperty("user.name") + "\n");
- 
-            // server replies with OK after printing out a welcome message and writing system info
- 
-            // setLargestServer();
- 
-            System.out.println("Sending REDY ...");
 
-            writeBytes(REDY);
+            // server replies with OK
+
+            // System.out.println("sent AUTH username");
+            writeBytes(AUTH + " " + System.getProperty("user.name"));
+
+            stringBuffer = bfr.readLine();
             
-            System.out.println("REDY sent.");
- 
-            readStringBuffer(); // reset stringBuffer & read job
- 
-            while (!stringBuffer.contains(NONE)){
+
+            // server replies with OK after printing out a welcome message and writing system info
+
+            // System.out.println("Sending REDY ...");
+            writeBytes(REDY);
+            // System.out.println("REDY sent.");
+            
+
+            // System.out.println("---------------");
+            while (!(stringBuffer = bfr.readLine()).contains(NONE)) {
                 
                 if (stringBuffer.contains(JOBN)) {
+                    // STORE JOB DATA
+                    // System.out.println(stringBuffer); // print JOB info
                     fieldBuffer = stringBuffer.split(" "); /* split String into array of strings
                                                               (each string being a field of JOBN) */
- 
+
                     Job job = new Job(fieldBuffer); // create new Job object with data from fieldBuffer
-                    
-                    System.out.println(stringBuffer);
 
-                    writeBytes("GETS Capable " + job.core + " " + job.memory + " " + job.disk + "\n");
-                    
-                    writeBytes(OK);
-                    
-                    readStringBuffer();
-                    System.out.println("DATA received : " + stringBuffer);
- 
-                    fieldBuffer = stringBuffer.split(" "); // fieldBuffer[1] -> no. of capable servers
-                    int numCapableServer = Integer.parseInt(fieldBuffer[1]);
-                    System.out.println("dtainfo"+" "+numCapableServer);
 
-                    writeBytes(OK); // send list of capable server (one at a time)
-                    
-                    
-                   for(int i=0;i<numCapableServer;i++){
-                    readStringBuffer();
-                    fieldBuffer=stringBuffer.split(" "); 
-                    String type = fieldBuffer[0];
-                    int id = Integer.parseInt(fieldBuffer[1]);
-                    String state = fieldBuffer[2];
-                    int curStartTime = Integer.parseInt(fieldBuffer[3]);
-                    int core = Integer.parseInt(fieldBuffer[4]);
-                    int memory = Integer.parseInt(fieldBuffer[5]);
-                    int disk = Integer.parseInt(fieldBuffer[6]); 
-                    int wjobs = Integer.parseInt(fieldBuffer[7]);
-                    int rjobs = Integer.parseInt(fieldBuffer[8].trim());   
-                    Server s = new Server(type, id, state, curStartTime, core, memory, disk, wjobs, rjobs);
-                    serverList.add(s);
-                    System.out.println(stringBuffer);
-                   }
+                    // get list of capable servers (state information)
+                    writeBytes(GETS + " Capable " + job.core + " " + job.memory + " " + job.disk);
 
+                    // DATA _ _ message
+                    stringBuffer = bfr.readLine();
+                    // System.out.println("DATA received : " + stringBuffer);
+                    fieldBuffer = stringBuffer.split(" "); 
+                    int numCapableServer = Integer.parseInt(fieldBuffer[1]); // fieldBuffer[1] -> no. of capable servers
+
+                    writeBytes(OK); // confirmation for receiving DATA
+
+                    ArrayList<Server> serverList = new ArrayList<>();
+                    // System.out.println("* * List of capable servers * *");
+                    for (int i = 0; i < numCapableServer; i++) {
+                        stringBuffer = bfr.readLine(); // read single server information
+                        
+                        fieldBuffer = stringBuffer.split(" ");
+                        String type = fieldBuffer[0];
+                        int id = Integer.parseInt(fieldBuffer[1]);
+                        String state = fieldBuffer[2];
+                        int curStartTime = Integer.parseInt(fieldBuffer[3]);
+                        int core = Integer.parseInt(fieldBuffer[4]);
+                        int memory = Integer.parseInt(fieldBuffer[5]);
+                        int disk = Integer.parseInt(fieldBuffer[6]);
+                        int wjobs = Integer.parseInt(fieldBuffer[7]);
+                        int rjobs = Integer.parseInt(fieldBuffer[8].trim());
+                        Server s = new Server(type, id, state, curStartTime, core, memory, disk, wjobs, rjobs);
+                        serverList.add(s);
+                        // System.out.println(stringBuffer);
+                    }
+                    writeBytes(OK); 
+                    stringBuffer = bfr.readLine();
+
+
+                    // ALGORITHM FOR JOB SCHEDULING
+                    // determines which server each job is sent/scheduled to
+                    Server s = mostEfficientServer(serverList, job.core, job.disk, job.memory);
+                    // System.out.println(s.type + " " + s.id);
+                    serverList.clear();
+
+                    /* SCHEDULE JOB */
                    
-                    writeBytes(OK);
+                    scheduleString = SCHD + " " + job.id + " " + s.type + " " + s.id ;
+                    writeBytes(scheduleString);
                    
-                   Server s = mostEfficientServer(serverList, job.core,job.disk,job.memory);
-                   System.out.println(s.type+" "+s.id);
-                   scheduleString = SCHD +" "+job.id+" "+s.type+" "+s.id+"\n";
-                   writeBytes(scheduleString);
-                   
-                   serverList.clear();// delete all items to read for next list from GETS Capable
+                    stringBuffer = bfr.readLine();
 
-                   readStringBuffer();
+                    writeBytes(REDY);
+                    
+                    // request new job
+                     // send REDY for the next job
                    
-                   writeBytes(REDY);
-
-                   readStringBuffer();
+                } 
+                else if (stringBuffer.contains(JCPL)) {
+                    writeBytes(REDY);
                 }
-                if (stringBuffer.contains(JCPL)) {
-                    writeBytes(REDY); // send REDY for the next job
-                }
+            }
 
-                //    if (stringBuffer.contains("OK")) {
-                //     readStringBuffer(); // reset stringBuffer & read next job
-                // } 
-                readStringBuffer();
-            
-        }
-                
-                
-            System.out.println("TERMINATING CONNECTION ...");
+            // System.out.println("TERMINATING CONNECTION ...");
             
             writeBytes(QUIT);
- 
-            System.out.println("CONNECTION TERMINATED.");
- 
+
+            // System.out.println("CONNECTION TERMINATED.");
+
             close();
-    }   
-         catch (UnknownHostException e) {
+        } catch (UnknownHostException e) {
             System.out.println("Unknown Host Exception: " + e.getMessage());
         } catch (EOFException e) {
             System.out.println("End of File Exception: " + e.getMessage());
@@ -152,48 +148,19 @@ public class Client {
             System.out.println("Exception: " + e.getMessage());
         }
     }
- 
+
     public static void setup() throws IOException {
-        serverList = new ArrayList<>(); // initialise list of servers
- 
         s = new Socket(hostname, serverPort); // socket with host IP of 127.0.0.1 (localhost), server port of 50000
- 
+
         din = new InputStreamReader(s.getInputStream());
         bfr = new BufferedReader(din);
         dout = new DataOutputStream(s.getOutputStream());
     }
- 
-    public static void writeBytes(String command) throws IOException {
-        byteBuffer = command .getBytes();
-        dout.write(byteBuffer);
+
+    public static void writeBytes(String message) throws IOException {
+        dout.write((message + "\n").getBytes());
         dout.flush();
     }
-
- 
-    //Algorithm used to select the serverList -> modification of first fit
-    //We check servers available resources and than give preferance to servers that are already active or idle
-    //if no capable server is active, we schedule job to first capable servers
-    /*******  ALGORITHM *********/
-    public static Server mostEfficientServer(List<Server> s, int core, int memory, int disk){
-        List<Server> tempList = new ArrayList<Server>();
-        for(Server server:s){
-            if(server.core>=core && server.memory>=memory && server.disk>=disk ){
-                tempList.add(server);
-            }
-        }
-        for(Server server:tempList){
-            if(server.state.equals("active") || server.state.equals("idle")){
-                return server;
-            }
-        }
-        return tempList.get(0);
-    }
-       
-
-    public static void readStringBuffer() throws IOException {
-        stringBuffer = bfr.readLine();
-    }
- 
     
     public static void close() throws IOException {
         bfr.close();
@@ -202,7 +169,43 @@ public class Client {
         s.close();
     }
 
-
-
-
+    public static Server mostEfficientServer(List<Server> s, int core, int memory, int disk) {
+        List<Server> idleList = new ArrayList<Server>();
+        List<Server> activeList = new ArrayList<Server>();
+        List<Server> tempList = new ArrayList<Server>();
+        for (Server server : s) {
+            if (server.core >= core && server.memory >= memory && server.disk >= disk) {
+                if(server.state.equals("idle")){
+                  idleList.add(server);  
+                }
+                else if(server.state.equals("active")){
+                    activeList.add(server);
+                }
+                else {
+                    tempList.add(server);
+                }
+                
+            }
+        }
+        if(idleList.size()>0){
+        for (Server server : idleList) {
+            if(server.core==core) return server;
+            }
+            return idleList.get(0);
+        }
+        if(activeList.size()>0){
+            for (Server server : activeList) {
+                if(server.core==core) return server;
+                }
+                return activeList.get(0);
+            }
+        if(tempList.size()>0){
+            for (Server server : tempList) {
+                if(server.core==core) return server;
+                }
+                return tempList.get(0);
+            }
+    
+        return s.get(0);
+    }
 }
